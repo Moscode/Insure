ac88eba8887eaadeb145cc434015bf8d71ec7ffb
odunosho moses <odunoshomoses@gmail.com>
2021-10-03 18:35:04 +0100

    Javascript Media Query is added to reset the hambuger style

--- index.js
+++ index.js
@@ -3,11 +3,20 @@ const closeMenu = document.querySelector("#close-menu");
 const menu = document.querySelector(".menu");
 const body = document.body;
 
+
+function resetingHambugerMenu(menuType) {
+  const mediaQuery = window.matchMedia("(min-width: 768px)");
+  if (mediaQuery.matches) {
+    menuType.style.display = "none";
+  }
+}
+
 function open() {
   menu.style.left = "0%";
   openMenu.style.display = "none";
   closeMenu.style.display = "inline-block";
   body.style.position = "fixed";
+  resetingHambugerMenu(closeMenu);
 }
 
 function close() {
@@ -15,6 +24,7 @@ function close() {
   closeMenu.style.display = "none";
   openMenu.style.display = "inline-block";
   body.style.position = "relative";
+  resetingHambugerMenu(openMenu);
 }
 
 closeMenu.addEventListener("click", close);
