1b345918ca66bac2b59e6f63c2ab4d20323f8c45
odunosho moses <odunoshomoses@gmail.com>
2021-10-05 17:08:16 +0100

    Fixed error with resizing the screen

--- index.js
+++ index.js
@@ -3,25 +3,11 @@ const closeMenu = document.querySelector("#close-menu");
 const menu = document.querySelector(".menu");
 const body = document.body;
 
-
-function resetingHambugerMenuStyle(menuType) {
-  const mediaQuery = window.matchMedia("(min-width: 768px)");
-
-  function handleScreenResize(e) {
-    if (e.matches) {
-      menuType.style.display = "none";
-    }
-  }
-  mediaQuery.addEventListener("resize", handleScreenResize);
-  handleScreenResize(mediaQuery);
-}
-
 function open() {
   menu.style.left = "0%";
   openMenu.style.display = "none";
   closeMenu.style.display = "inline-block";
   body.style.position = "fixed";
-  resetingHambugerMenuStyle(closeMenu);
 }
 
 function close() {
@@ -29,7 +15,7 @@ function close() {
   closeMenu.style.display = "none";
   openMenu.style.display = "inline-block";
   body.style.position = "relative";
-  resetingHambugerMenuStyle(openMenu);
+  // resetingHambugerMenu(openMenu);
 }
 
 closeMenu.addEventListener("click", close);
